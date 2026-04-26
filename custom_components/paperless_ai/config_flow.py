9c47cffa74031de6a320574ae39322164b05b696
grassiekuik <grassiekuik@gmail.com>
2026-04-26 11:46:36 +0200

    updates

--- custom_components/paperless_ai/config_flow.py
+++ custom_components/paperless_ai/config_flow.py
@@ -1,33 +1,19 @@
 import voluptuous as vol
 from homeassistant import config_entries
-import homeassistant.helpers.config_validation as cv
 from .const import DOMAIN, CONF_HOST, CONF_API_KEY
 
 class PaperlessAIConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Beheer de configuratie flow voor Paperless-AI."""
-
+    """Handle a config flow for Paperless-AI."""
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Stap die wordt aangeroepen wanneer de gebruiker de integratie toevoegt."""
-        errors = {}
-
         if user_input is not None:
-            # Hier maken we de koppeling definitief
-            return self.async_create_entry(
-                title="Paperless-AI", 
-                data={
-                    CONF_HOST: user_input[CONF_HOST],
-                    CONF_API_KEY: user_input[CONF_API_KEY],
-                }
-            )
+            return self.async_create_entry(title="Paperless-AI", data=user_input)
 
-        # Het formulier met de exacte velden
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required(CONF_HOST, default="http://192.168.50.39:3000"): str,
                 vol.Required(CONF_API_KEY): str,
-            }),
-            errors=errors,
+            })
         )
